416a0255e9d55649ef6da3f26b00be18f346fe69
FGOSousa <122644887+FGOSousa@users.noreply.github.com>
2024-04-22 14:52:26 -0300

    Add files via upload

---
+++ provasuper1/main.py
@@ -0,0 +1,15 @@
+
+email='construcaoadm@gmail.com'
+senha='cons1234'
+
+while True:
+    emailcad=input('Digite seu email: ')
+    if emailcad==email:
+        senhacad=input('Digite sua senha: ')
+        if senhacad==senha:
+            print('Acesso permitido')
+            break
+
+
+
+    
